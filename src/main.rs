35bde12e74fb27eb6e5ca73d985f4aa8732b793b
Danilo Tamindzija <danilotamindzija45@gmail.com>
2025-09-30 15:56:52 +0200

    Initial commit: Hello World program

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("Hello, World from Rust!!!");
+    println!("println without ! will cause error, because ! implicate that println is macro");
+    println!("name of the fn is case sensitive so if I rename main to Main it will cause error")
+}
