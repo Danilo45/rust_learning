7299f97b2fa49246192653f4ea9f4adb03ccb519
Danilo Tamindzija <danilotamindzija45@gmail.com>
2025-10-03 14:56:16 +0200

    Organized Week1 project

--- src/main.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    println!("Hello, World from Rust!!!");
-    println!("println without ! will cause error, because ! implicate that println is macro");
-    println!("name of the fn is case sensitive so if I rename main to Main it will cause error")
-}
---
+++ week1/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Hello, World from Rust!!!");
+    println!("`println!` without `!` will cause an error, because `!` means it is a macro.");
+    println!("The function name is case-sensitive, so if I rename `main` to `Main` it will cause an error.");
+    println!("Rust is a memory-safe programming language. I want to learn it because it feels safer than C and C++ in terms of memory.");
+    println!("It doesn't let you make common mistakes like null or dangling pointers.");
+}
