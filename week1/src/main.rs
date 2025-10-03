92538ba4840b7c23af3d993110d83fb935c5e745
Danilo Tamindzija <danilotamindzija45@gmail.com>
2025-10-03 16:04:40 +0200

    Experimenting with different print-related macros

--- week1/src/main.rs
+++ week1/src/main.rs
@@ -4,4 +4,21 @@ fn main() {
     println!("The function name is case-sensitive, so if I rename `main` to `Main` it will cause an error.");
     println!("Rust is a memory-safe programming language. I want to learn it because it feels safer than C and C++ in terms of memory.");
     println!("It doesn't let you make common mistakes like null or dangling pointers.");
+    print!("This is different macro, it does not add new line at the end.");
+    print!("\n");
+    print!("Hello ");
+    eprintln!("An error occurred: invalid input");
+    let name = "Danilo";
+    let age = 24;
+    let message = format!("My name is {} and I am {} yo.", name, age);
+    println!("{}", message);
+    let msg2 = format!(
+        "We can do it \
+        this way also:
+        name: {my_name},
+        age: {my_age}",
+        my_age=age,
+        my_name= name
+    );
+    println!("{}", msg2);
 }
---
+++ week1/target/debug/deps/hello_world-eb6e24829faf2506.d
@@ -0,0 +1,5 @@
+C:\Users\danilo.tamindzija\hello_world\week1\target\debug\deps\hello_world-eb6e24829faf2506.d: src\main.rs
+
+C:\Users\danilo.tamindzija\hello_world\week1\target\debug\deps\libhello_world-eb6e24829faf2506.rmeta: src\main.rs
+
+src\main.rs:
